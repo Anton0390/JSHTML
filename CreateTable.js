776b0d0e2d2df03c3a6997abba140555baa16c46
Anton0390 <95080723+Anton0390@users.noreply.github.com>
2022-05-20 14:08:42 +0300

    Update CreateTable.js

--- CreateTable.js
+++ CreateTable.js
@@ -1,39 +1,42 @@
 function tableCreate(thead, tbody) {
-        
-        let body = document.getElementsByTagName('body')[0];
-        let tbl = document.createElement('table');
-        tbl.style.width = '100%';
-        tbl.style['border-style'] = 'solid';
-        tbl.style['border-collapse'] = 'collapse';
-        tbl.setAttribute('border', '1');
-        let tbdy = document.createElement('tbody');
-        let tbhd = document.createElement('thead');
-        let tr = document.createElement('tr');
-
-        //header
-
-        thead.forEach(cell => {
-            let th = document.createElement('th');
-            th.style.background = '#b2b0b0'
-            th.appendChild(document.createTextNode(cell))
-            tr.appendChild(th)
-            tbhd.appendChild(tr);
-        })
-        tbl.appendChild(tbhd);
-
-        //tbody
-
-        tbody.forEach(row => {
+            if (!thead) thead = ['test', 'test']
+            if (!tbody) thead = [
+                ['test', 'test'],
+                ['test', 'test']
+            ]
+            let body = document.getElementsByTagName('body')[0];
+            let tbl = document.createElement('table');
+            tbl.style.width = '100%';
+            tbl.style['border-style'] = 'solid';
+            tbl.style['border-collapse'] = 'collapse';
+            tbl.setAttribute('border', '1');
+            let tbdy = document.createElement('tbody');
+            let tbhd = document.createElement('thead');
             let tr = document.createElement('tr');
-            row.forEach(cell => {
-                let td = document.createElement('td');
-                td.style['text-align'] = 'center'
-                td.appendChild(document.createTextNode(cell))
-                tr.appendChild(td)
+
+            //header
+
+            thead.forEach(cell => {
+                let th = document.createElement('th');
+                th.style.background = '#b2b0b0'
+                th.appendChild(document.createTextNode(cell))
+                tr.appendChild(th)
+                tbhd.appendChild(tr);
             })
-            tbdy.appendChild(tr);
-        })
-        tbl.appendChild(tbdy);
-        //body.appendChild(tbl)
-        body.replaceChild(tbl, body.childNodes[body.childNodes.length-1]);
-    }
+            tbl.appendChild(tbhd);
+
+            //tbody
+
+            tbody.forEach(row => {
+                let tr = document.createElement('tr');
+                row.forEach(cell => {
+                    let td = document.createElement('td');
+                    td.style['text-align'] = 'center'
+                    td.appendChild(document.createTextNode(cell))
+                    tr.appendChild(td)
+                })
+                tbdy.appendChild(tr);
+            })
+            tbl.appendChild(tbdy);
+            body.replaceChild(tbl, body.childNodes[body.childNodes.length - 1]);
+        }

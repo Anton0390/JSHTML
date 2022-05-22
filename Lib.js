45dfea371ec73f7b9563e12ab44a00d3da3a8670
Anton0390 <95080723+Anton0390@users.noreply.github.com>
2022-05-22 11:29:13 +0300

    Update Lib.js
    Add the round function

--- Lib.js
+++ Lib.js
@@ -41,3 +41,19 @@ function tableCreate(thead, tbody) {
             tbl.appendChild(tbdy);
             body.replaceChild(tbl, body.childNodes[body.childNodes.length - 1]);
         }
+
+/**
+ * return the number rounded to the places.
+ *
+ * @param {Number} num Число которое буден округлено.
+ * @param {Number} places Кол-во разрядов до которого будет окурглено число.
+ * @param {String} replace Если num не число, то оно заменяется на replace.
+ * @return the number round up or down to the places.
+ */
+
+function round(num, places, replace) {
+  if (!places && places !== 0) places = 3
+  if (!replace) replace = '-'
+  if (isNaN(Number(num)) || num === '') return replace
+  else return +(Math.round(+num + "e+" + places) + "e-" + places);
+}

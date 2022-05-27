d056719760a62433ecfa4da5ba08a99a68c7cc89
Anton0390 <95080723+Anton0390@users.noreply.github.com>
2022-05-27 08:37:39 +0300

    Update Lib.js

--- Lib.js
+++ Lib.js
@@ -6,10 +6,10 @@ function tableCreate(thead, tbody) {
             ]
             let body = document.getElementsByTagName('body')[0];
             let tbl = document.createElement('table');
-            tbl.style.width = '100%';
-            tbl.style['border-style'] = 'solid';
-            tbl.style['border-collapse'] = 'collapse';
-            tbl.setAttribute('border', '1');
+            //tbl.style.width = '100%';
+            //tbl.style['border-style'] = 'solid';
+            //tbl.style['border-collapse'] = 'collapse';
+            //tbl.setAttribute('border', '1');
             let tbdy = document.createElement('tbody');
             let tbhd = document.createElement('thead');
             let tr = document.createElement('tr');
@@ -18,8 +18,8 @@ function tableCreate(thead, tbody) {
 
             thead.forEach(cell => {
                 let th = document.createElement('th');
-                th.style.background = '#b2b0b0'
-                th.style['text-align'] = 'center';
+                //th.style.background = '#b2b0b0'
+                //th.style['text-align'] = 'center';
                 th.appendChild(document.createTextNode(cell))
                 tr.appendChild(th)
                 tbhd.appendChild(tr);
@@ -32,7 +32,7 @@ function tableCreate(thead, tbody) {
                 let tr = document.createElement('tr');
                 row.forEach(cell => {
                     let td = document.createElement('td');
-                    td.style['text-align'] = 'center'
+                    //td.style['text-align'] = 'center'
                     td.appendChild(document.createTextNode(cell))
                     tr.appendChild(td)
                 })

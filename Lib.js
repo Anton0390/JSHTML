d97c84813a54472a5d33595d9b4a1bf0605b8312
Anton0390 <95080723+Anton0390@users.noreply.github.com>
2022-06-03 13:36:14 +0300

    Update Lib.js

--- Lib.js
+++ Lib.js
@@ -1,52 +1,58 @@
 /**
- * Create or refresh the table.
- *
- * @param {Array} thead The head of the table.
- * @param {Array<Array>} tbody The body of the table.
- * @param {String} tableId Set Id of the table.
- * @param {HTMLelement} body HTML Element to append/replace the table.
- * @return The HTML table.
- */
-function tableCreate(thead, tbody, tableId, body) {
-  
-  let tbl = document.createElement("table");
-  tbl.setAttribute("id", tableId.toString());
+     * Create or refresh the table.
+     *
+     * @param {Array} thead The head of the table.
+     * @param {Array<Array>} tbody The body of the table.
+     * @param {String} tableId Set Id of the table.
+     * @param {HTMLelement} body HTML Element to append/replace the table.
+     * @return The HTML table.
+     */
+    function tableCreate(thead, tbody, tableId, parent) {
+        if (!parent) parent = document.getElementsByTagName('body')
+        if (!tableId) tableId = 'tableId'
+        if (!tbody) tbody = [
+            []
+        ]
+        let tbl = document.createElement("table");
+        tbl.setAttribute("id", tableId.toString());
+        tbl.setAttribute("class", 'AStable');
 
-  let tbdy = document.createElement("tbody");
-  let tbhd = document.createElement("thead");
-  let tr = document.createElement("tr");
+        let tbdy = document.createElement("tbody");
+        let tbhd = document.createElement("thead");
+        let tr = document.createElement("tr");
 
-  //header
+        //header
 
-  thead.forEach((cell) => {
-    let th = document.createElement("th");
-    th.appendChild(document.createTextNode(cell));
-    tr.appendChild(th);
-    tbhd.appendChild(tr);
-  });
-  tbl.appendChild(tbhd);
+        thead.forEach((cell) => {
+            let th = document.createElement("th");
+            th.appendChild(document.createTextNode(cell));
+            tr.appendChild(th);
+            tbhd.appendChild(tr);
+        });
+        tbl.appendChild(tbhd);
 
-  //tbody
+        //tbody
 
-  tbody.forEach((row) => {
-    let tr = document.createElement("tr");
-    row.forEach((cell) => {
-      let td = document.createElement("td");
-      td.appendChild(document.createTextNode(cell));
-      tr.appendChild(td);
-    });
-    tbdy.appendChild(tr);
-  });
-  tbl.appendChild(tbdy);
+        tbody.forEach((row) => {
+            let tr = document.createElement("tr");
+            row.forEach((cell) => {
+                let td = document.createElement("td");
+                td.appendChild(document.createTextNode(cell));
+                tr.appendChild(td);
+            });
+            tbdy.appendChild(tr);
+        });
+        tbl.appendChild(tbdy);
 
-  let tablePos;
-  Array.prototype.slice.call(body[0].childNodes).forEach((el, i) => {
-    if (el.id == tableId) tablePos = i;
-  });
-  tablePos
-    ? body[0].replaceChild(tbl, body[0].childNodes[tablePos])
-    : body[0].appendChild(tbl);
-} 
+        let tablePos;
+        Array.prototype.slice.call(parent[0].childNodes).forEach((el, i) => {
+            if (el.id == tableId) tablePos = i;
+        });
+        tablePos
+            ?
+            parent[0].replaceChild(tbl, parent[0].childNodes[tablePos]) :
+            parent[0].appendChild(tbl);
+    }
 
 /**
  * return the number rounded to the places.
